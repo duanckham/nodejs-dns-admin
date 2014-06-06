8fed7bacf4ba735131ea3dfe0c4a0723ef59ec28
noevil <noevil@foxmail.com>
2014-06-06 15:58:39 +0800

    - some change

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var app = express();
 
 app.use(bodyParser());
 app.use('/', router);
-app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/html'));
+app.use(express.static(__dirname + '/public'));
 app.listen(CONFIG.PORT);
 
 console.log('; api service running.');
